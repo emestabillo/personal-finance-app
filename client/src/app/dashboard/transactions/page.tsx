6180348220f223f0b7f042434fef5bd4c57c9bc1
Emmilie Estabillo <emsestabillo@gmail.com>
2025-05-02 14:21:49 -0400

    Add dialog component

--- client/src/app/dashboard/transactions/page.tsx
+++ client/src/app/dashboard/transactions/page.tsx
@@ -1,14 +1,16 @@
 import TransactionsClient from "@/app/dashboard/transactions/TransactionsClient";
 import ProtectedRoute from "@/components/ProtectedRoute";
+import TransactionModal from "./TransactionModal";
 
 export default function TransactionsPage() {
   return (
     <ProtectedRoute>
       <div className="p-6">
-        <div>
-          <h1 className="text-2xl font-bold mb-4">Transactions</h1>
-          <TransactionsClient />
-        </div>
+        <header className="flex justify-between mb-4">
+          <h1 className="text-2xl font-bold">Transactions</h1>
+          <TransactionModal />
+        </header>
+        <TransactionsClient />
       </div>
     </ProtectedRoute>
   );
